01b285767f5a664ecdb65e7581af7b01a8ea06c1
renesiodlaczek <rene.siodlaczek@t-online.de>
2023-01-27 21:47:13 +0100

    style adaption

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -1,18 +1,18 @@
 #' Read a Fatality Analysis Reporting System data file
 #'
-#' This function reads a Fatality Analysis Reporting System .csv data file and 
+#' This function reads a Fatality Analysis Reporting System .csv data file and
 #' returns the data in a \code{tibble} format.
 #'
 #' @param filename A character string giving the name of the data file.
-#' 
+#'
 #' @return This function returns the read data in a \code{tibble} format.
-#' 
-#' @note The function execution is stopped if no file with the provided 
+#'
+#' @note The function execution is stopped if no file with the provided
 #'      filename exists in the working directory.
-#' 
-#' @examples 
+#'
+#' @examples
 #' accident_2013 <- fars_read("accident_2013.csv.bz2")
-#' 
+#'
 #' @export
 fars_read <- function(filename) {
         if(!file.exists(filename))
@@ -26,22 +26,22 @@ fars_read <- function(filename) {
 
 #' Make a filename for a Fatality Analysis Reporting System "csv.bz2"-file
 #'
-#' This function creates a character vector to create a Fatality Analysis Reporting 
-#' System filename. The single values consists of "accident", the parameter 
+#' This function creates a character vector to create a Fatality Analysis Reporting
+#' System filename. The single values consists of "accident", the parameter
 #' \code{year} and the ending "csv.bz2".
 #'
 #' @param year A character or integer vector with the years that are
 #'      displayed in the filenames.
-#' 
+#'
 #' @note The function execution is stopped if the given parameter cannot be
 #'      coerced into a integer value or vector.
-#' 
+#'
 #' @return This function returns a character vector with the created filenames.
-#' 
-#' @examples 
+#'
+#' @examples
 #' filename_2013 <- make_filename("2013")
 #' filename_2013_2014 <- make_filename(c(2013, 2014))
-#' 
+#'
 #' @export
 make_filename <- function(year) {
         year <- as.integer(year)
@@ -51,28 +51,28 @@ make_filename <- function(year) {
 
 #' Read multiple Fatality Analysis Reporting System "csv.bz2"-files
 #'
-#' This function reads multiple Fatality Analysis Reporting System "csv.bz2"-files 
+#' This function reads multiple Fatality Analysis Reporting System "csv.bz2"-files
 #' and returns a list with the corresponding tibbles that contain the month and
 #' year of the original data entries. The parameter \code{years} specifies from
 #' which years the data shall be read.
 #'
 #' @param years A character or integer vector that contains the years from which
 #'      the data shall be read.
-#' 
-#' @return This function returns a list of tibbles that contain the month and 
+#'
+#' @return This function returns a list of tibbles that contain the month and
 #'      year of the original data entries.
-#' 
-#' @details The function calls the functions \code{\link{make_filename}} and 
+#'
+#' @details The function calls the functions \code{\link{make_filename}} and
 #'      \code{\link{fars_read}}.
-#' 
-#' @note The function execution is stopped if the filename creation fails or if 
-#'      no corresponding file for the generated filename exists in the working 
+#'
+#' @note The function execution is stopped if the filename creation fails or if
+#'      no corresponding file for the generated filename exists in the working
 #'      directory.
-#' 
-#' @examples 
+#'
+#' @examples
 #' fars_month_year <- fars_read_years(c(2013, 2014, 2015))
 #' fars_month_year <- fars_read_years(c("2013", "2014", "2015"))
-#' 
+#'
 #' @importFrom dplyr %>%
 #'
 #' @export
@@ -81,7 +81,7 @@ fars_read_years <- function(years) {
                 file <- make_filename(year)
                 tryCatch({
                         dat <- fars_read(file)
-                        dplyr::mutate(dat, year = year) %>% 
+                        dplyr::mutate(dat, year = year) %>%
                                 dplyr::select(MONTH, year)
                 }, error = function(e) {
                         warning("invalid year: ", year)
@@ -93,32 +93,32 @@ fars_read_years <- function(years) {
 
 #' Summarize Fatality Analysis Reporting System data
 #'
-#' This function summarizes multiple Fatality Analysis Reporting System 
-#' "csv.bz2"-files after reading them in. For every year the count of incidents 
-#' per month is returned. The parameter \code{years} specifies from which years 
+#' This function summarizes multiple Fatality Analysis Reporting System
+#' "csv.bz2"-files after reading them in. For every year the count of incidents
+#' per month is returned. The parameter \code{years} specifies from which years
 #' the data shall be read.
 #'
 #' @param years A character or integer vector that contains the years from which
 #'      the data shall be read and summarized.
-#' 
-#' @return This function returns a tibble in the wide-format with one column per 
-#'      year and one row per month. The values are the corresponding counts of 
+#'
+#' @return This function returns a tibble in the wide-format with one column per
+#'      year and one row per month. The values are the corresponding counts of
 #'      incidents.
-#' 
+#'
 #' @details The function calls the function \code{\link{fars_read_years}}.
-#' 
-#' @examples 
+#'
+#' @examples
 #' fars_summarize_years(c(2013, 2014, 2015))
 #' fars_summarize_years(c("2013", "2014", "2015"))
-#' 
+#'
 #' @importFrom dplyr %>%
 #' @importFrom dplyr n
-#' 
+#'
 #' @export
 fars_summarize_years <- function(years) {
         dat_list <- fars_read_years(years)
-        dplyr::bind_rows(dat_list) %>% 
-                dplyr::group_by(year, MONTH) %>% 
+        dplyr::bind_rows(dat_list) %>%
+                dplyr::group_by(year, MONTH) %>%
                 dplyr::summarize(n = n()) %>%
                 tidyr::spread(year, n)
 }
@@ -129,24 +129,24 @@ fars_summarize_years <- function(years) {
 #' This function creates a map with the geo-locations of the accidents for the
 #' given state and given year.
 #'
-#' @param state.num A character or integer value of the state number for which 
+#' @param state.num A character or integer value of the state number for which
 #'      the accidents shall be mapped.
-#' @param years A character or integer value of the year for which the data shall 
+#' @param years A character or integer value of the year for which the data shall
 #'      be read and mapped.
-#' 
-#' @return This function returns a map with the geo-locations of the accidents 
+#'
+#' @return This function returns a map with the geo-locations of the accidents
 #'      that happened in the requested state and year.
-#' 
-#' @details The function calls the functions \code{\link{make_filename}} and 
+#'
+#' @details The function calls the functions \code{\link{make_filename}} and
 #'      \code{\link{fars_read}}.
-#' 
-#' @note The function execution is stopped if an invalid state number or year is 
+#'
+#' @note The function execution is stopped if an invalid state number or year is
 #'      given or when there is no data to plot.
-#' 
-#' @examples 
+#'
+#' @examples
 #' fars_map_state(1, 2015)
 #' fars_map_state("1", "2015")
-#' 
+#'
 #' @export
 fars_map_state <- function(state.num, year) {
         filename <- make_filename(year)
@@ -168,3 +168,4 @@ fars_map_state <- function(state.num, year) {
                 graphics::points(LONGITUD, LATITUDE, pch = 46)
         })
 }
+
